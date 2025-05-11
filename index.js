a5480f9911f6327b91f643ea5d1447a621fc8754
etharialle <christopher.m.adams2@gmail.com>
2025-05-11 07:41:24 -0700

    basic functions working, need to refactor to a final function

--- index.js
+++ index.js
@@ -19,19 +19,28 @@ function generateLegalMoves(currentPosition) {
 }
 
 function addMovesToQueue(currentPosition, legalMoves, cost) {
-    for (let i = 0; i < legalMoves.length; i++) {
-        let newQueueItem = [currentPosition, legalMoves[i], cost];
-        graphQueue.push([currentPosition,legalMoves[i],cost]);
+    for (let j = 0; j < legalMoves.length; j++) {
+        let newQueueItem = [currentPosition, legalMoves[j], cost];
+        graphQueue.push([currentPosition,legalMoves[j],cost]);
     }
 }
 
-function addPathToMatrix(graphQueue) {
-    while (graphQueue.length > 0) {
+function addPathToMatrix(graphQueue, targetPosition) {
+    let k = 0;
+    while (graphQueue.length > 0 && !(targetPosition.toString() in adjacencyMatrix)) {
+        console.log(Object.keys(adjacencyMatrix));
+        console.log(targetPosition);
+        console.log(targetPosition.toString());
         if (!(graphQueue[0][1] in Object.keys(adjacencyMatrix))) {
             let newKey = graphQueue.shift();
-            adjacencyMatrix[newKey[1]] = [newKey[0], newKey[2]];
+            adjacencyMatrix[newKey[1].toString()] = [newKey[0], newKey[2]];
+            let newLegalMoves = generateLegalMoves(newKey[1]);
+            let newCost = newKey[2] + 1;
+            addMovesToQueue(newKey[1], newLegalMoves,newCost)
         }
         console.log(graphQueue);
+        console.log(adjacencyMatrix);
+        k++;
     }
     
 }
@@ -39,7 +48,7 @@ function addPathToMatrix(graphQueue) {
 let graphQueue = []; // parent, next, cost
 let cost = 1;
 const currentPosition = [0,0];
-const targetPositionn = [3,3];
+const targetPosition = [3,3];
 let pathArray = [currentPosition];
 let costArray = [0];
 let adjacencyMatrix = {};
@@ -49,7 +58,25 @@ let legalMoves = generateLegalMoves(currentPosition);
 console.log(legalMoves);
 addMovesToQueue(currentPosition, legalMoves, cost);
 console.log(graphQueue);
-addPathToMatrix(graphQueue);
+addPathToMatrix(graphQueue, targetPosition);
 console.log(adjacencyMatrix);
 
+// create array of positions from final to start
+function getShortestPath(adjacencyMatrix, targetPosition, currentPosition) {
+    let pointer = targetPosition;
+    while (pointer != currentPosition) {
+        shortestPath.push(pointer);
+        pointer = adjacencyMatrix[pointer.toString()][0];
+        console.log(pointer);
+    }
+    shortestPath.push(currentPosition);
+}
+
+let shortestPath = [];
+getShortestPath(adjacencyMatrix, targetPosition, currentPosition);
+const shortestPathReversed = shortestPath.reverse();
+console.log(shortestPathReversed)
+shortestPathReversed.forEach(element => {
+    console.log(element);
+});
 module.exports = sumNumbers;
