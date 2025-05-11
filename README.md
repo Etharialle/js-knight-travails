e81c0c4585d26ad78ddf371185e2d63e811fbee3
etharialle <christopher.m.adams2@gmail.com>
2025-05-11 04:12:25 -0700

    generateLegalMoves function working

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # js-knight-travails
 Find the shortest path for a knight on a chess board to move from a selected start to a selected end position.
+
+
+# Plan
+
+- Graph connections could be created with a 64x64 2-D array (number of squares on a chess board)
+
+- rows and colums, with 0,0 being in the upper left
+- rows is an array [0-7] and columns is an array [0-7]
+- Possible moves for a knight (r, c)
+  - 1) (-1, -2)
+  - 2) (-1, 2)
+  - 3) (-2, 1)
+  - 4) (-2, -1)
+  - 5) (1, 2)
+  - 6) (1, -2)
+  - 7) (2, 1)
+  - 8) (2, -1
+   
+- Generate legal moves
+  ```
+  function generateLegalMoves(currentPosition)
+    legalMoves = []
+    moveList = [use above]
+    for move in moveList:
+        newPosition = [currentPosition[0]+move[0], currentPosition[1]+move[1]]
+        if newPosition.some(element => element > 7) || newPosition(element => element <0)
+```
+    
--- index.js
+++ index.js
@@ -2,4 +2,24 @@ function sumNumbers(num1, num2) {
     return num1 + num2;
 }
 
+function generateLegalMoves(currentPosition) {
+    const legalMoves = [];
+    const moveList = [[-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2], [2, -1], [2, 1]];
+    for (let i = 0; i < moveList.length; i++) {
+        //let x = currentPosition[0];
+        //let newX = moveList[i][0];
+        //const newRow = currentPosition[0] + moveList[i][0];
+        //const newColumn = currentPosition[1] + moveList[i][1];
+        let newPosition = [currentPosition[0]+moveList[i][0], currentPosition[1]+moveList[i][1]];
+        if (newPosition.every(element => element < 7) && newPosition.every(element => element > 0)) {
+            legalMoves.push(newPosition);
+        }
+    }
+    return legalMoves
+}
+
+const currentPosition = [0,0];
+
+console.log(generateLegalMoves(currentPosition))
+
 module.exports = sumNumbers;
